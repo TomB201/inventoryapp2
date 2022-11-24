b2728f7a3315aa3a61d44fbed648c0ada4aca477
TomB201 <tom.brchn@eduge.ch>
2022-11-24 09:36:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,13 @@
+//developpeur. Tom Bruchon
+
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
